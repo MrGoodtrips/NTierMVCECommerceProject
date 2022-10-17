28c25ede644db9a8b07de3dba02411868199b31a
Serhat Öztürk <serhatozturk.mr.goodtrips@gmail.com>
2022-10-17 17:05:33 +0300

    BLL katmanı tamamlandı.

---
+++ Project.BLL/DesignPatterns/SingletonPattern/DBTool.cs
@@ -0,0 +1,30 @@
+﻿using Project.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DesignPatterns.SingletonPattern
+{
+    public class DBTool
+    {
+
+        DBTool() { }
+
+        static MyContext _dbINstance;
+
+        public static MyContext DBINstance
+        {
+            get
+            {
+                if (_dbINstance == null)
+                {
+                    _dbINstance = new MyContext();
+                }
+                return _dbINstance;
+            }
+        }
+
+    }
+}
