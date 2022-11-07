4ec55eb308767729627e33aba4dd0bdeb4a673ff
Serhat Öztürk <serhatozturk.mr.goodtrips@gmail.com>
2022-11-07 16:34:55 +0300

    Register sayfasına başlandı.

--- Project.MVCUI/ConsumeDTO/PaymentDTO.cs
+++ Project.MVCUI/ConsumeDTO/PaymentDTO.cs
@@ -7,5 +7,12 @@ namespace Project.MVCUI.ConsumeDTO
 {
     public class PaymentDTO
     {
+        public int ID { get; set; }
+        public string CardUserName { get; set; }
+        public string SecurityNumber { get; set; }
+        public string CardNumber { get; set; }
+        public int CardExpiryMonth { get; set; }
+        public int CardExpiryYear { get; set; }
+        public decimal ShoppingPrice { get; set; }
     }
 }
---
+++ Project.MVCUI/Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using Project.BLL.DesignPatterns.GenericRepository.ConcRep;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.MVCUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        AppUserRepository _apRep;
+        AppUserProfileRepository _apProRep;
+
+        public RegisterController()
+        {
+            _apRep = new AppUserRepository();
+            _apProRep = new AppUserProfileRepository();
+        }
+
+        
+        public ActionResult RegisterNow()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegisterNow(AppUser appUser, AppUserProfile appUserProfile)
+        {
+            return View();
+        }
+    }
+}
